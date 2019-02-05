74097d7b1f8a2a26f04f1ac10637fd6929524bb8
Victor <vctr90w@gmail.com>
2019-02-04 23:56:57 -0400

    Cleaned up default express installation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.sendStatus(200);
 });
 
 module.exports = router;
