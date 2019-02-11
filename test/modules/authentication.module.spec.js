7ad32e52c09d109d04daf733dab42dd4bed0d465
Victor <vctr90w@gmail.com>
2019-02-10 22:15:32 -0400

    Added todo tests

---
+++ test/modules/authentication.module.spec.js
@@ -0,0 +1,13 @@
+describe('Authentication module', () => {
+  describe('A new user should be able to successfully sign in with a unique email', () => {
+    describe('User email should NOT be duplicated', () => {
+      test.todo('Validate email duplication');
+      test.todo('Reply duplication error message');
+    });
+
+    describe('User should be able to sign up', () => {
+      test.todo('Create user');
+      test.todo('Reply success message about user creation');
+    });
+  });
+});
