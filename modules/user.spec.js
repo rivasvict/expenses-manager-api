2a66fa2833a31cdb725fc29d50d1f21e8b3e29b1
Victor <vctr90w@gmail.com>
2019-02-24 18:45:22 -0400

    Improved some testing

--- modules/user.spec.js
+++ modules/user.spec.js
@@ -7,9 +7,12 @@ const userModule = rewire('./user.js');
 const signUp = userModule.__get__('signUp');
 const User = userModule.__get__('User');
 
-describe('Sign user up', function () {
+describe('Implementation: Sign user up', function () {
   beforeEach('Stub User model', function () {
     this.User = User.prototype;
+    this.setStub = function (stub) {
+      this.stub = stub;
+    };
   });
 
   it('Should create new user when it is formed with correct data', async function () {
@@ -19,16 +22,11 @@ describe('Sign user up', function () {
       lastName: 'Rivas',
       password: 'hola'
     };
-    const stub = sinon.stub(this.User, 'create')
-      .returns(Promise.resolve(userToTest));
-    try {
-      const user = await signUp(userToTest);
+    this.setStub(sinon.stub(this.User, 'create')
+      .returns(Promise.resolve(userToTest)));
+    const user = await signUp(userToTest);
 
-      expect(user).to.deep.equal(userToTest);
-      stub.restore();
-    } catch (error) {
-      console.log(error);
-    }
+    expect(user).to.deep.equal(userToTest);
   });
 
   it('Should throw error when missing data', async function () {
@@ -37,22 +35,19 @@ describe('Sign user up', function () {
       lastName: 'Rivas',
       password: 'hola'
     };
-    const stub = sinon.stub(this.User, 'create')
-      .throws();
+    this.setStub(sinon.stub(this.User, 'create')
+      .throws());
     try {
       await signUp(userToTest);
-
-      stub.restore();
     } catch (error) {
       expect(error.name).to.be.equal('ValidationError');
-      stub.restore();
     }
   });
 
   it('Should throw error when alrady used email', async function () {
     const duplicationUserError = new Error('Duplicated user');
-    const stub = sinon.stub(this.User, 'create')
-      .returns(Promise.resolve(duplicationUserError));
+    this.setStub(sinon.stub(this.User, 'create')
+      .returns(Promise.resolve(duplicationUserError)));
     const user = await signUp({
       firstName: 'Victor',
       email: 'ahfushaa',
@@ -61,6 +56,9 @@ describe('Sign user up', function () {
     });
 
     assert.equal(user, duplicationUserError);
-    stub.restore();
+  });
+
+  afterEach('Restore stubs', function () {
+    this.stub.restore();
   });
 });
