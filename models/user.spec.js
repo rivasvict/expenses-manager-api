989217644cff975c4e587441a7bec1a5b4f34ea4
Victor <vctr90w@gmail.com>
2019-02-24 21:52:33 -0400

    Added first unit test

--- models/user.spec.js
+++ models/user.spec.js
@@ -1,4 +1,33 @@
 const { expect } = require('chai');
+const { setupRecorder } = require('nock-record');
+
+const record = setupRecorder();
+
+const User = require('./user');
 const db = require('../db/');
 
-db.initialize();
+describe('Test for User model', function () {
+  beforeEach('Connect to DB', async function () {
+    await db.initialize();
+  });
+
+  describe('User class', function () {
+    beforeEach('Instance User class', function () {
+      this.user = new User({
+        password: 'myPassword',
+        lastName: 'myLastName',
+        email: 'asd',
+        firstName: 'myFirstName'
+      });
+    });
+
+    it('Should create user on db', async function () {
+      const { completeRecording, assertScopesFinished } = await record('create-regular-user');
+      const user = await this.user.create();
+
+      completeRecording();
+      assertScopesFinished();
+      expect(true).to.be.equal(true);
+    });
+  });
+});
