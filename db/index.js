87af13a26c20f8e69f38da6f5c9cad41881e6ff1
Victor <vctr90w@gmail.com>
2019-02-25 00:15:56 -0400

    Added improved environment variables

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const authenticatedMongoUrl = process.env.DEBUG ? '' : `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
+const authenticatedMongoUrl = `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
 const dbServer = process.env.DEBUG ? process.env.DB_TEST_SERVER : process.env.DB_SERVER;
 const dbPort = process.env.DEBUG ? process.env.DB_TEST_PORT : process.env.DB_PORT;
 const dbName = process.env.DEBUG ? process.env.DB_TEST_NAME : process.env.DB_NAME;
