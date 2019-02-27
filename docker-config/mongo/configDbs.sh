388cf20b005b9b01e8e936e03afa1a316b6fca7c
Victor <vctr90w@gmail.com>
2019-02-26 22:45:49 -0400

    Added script for test db

--- docker-config/mongo/configDbs.sh
+++ docker-config/mongo/configDbs.sh
@@ -1,7 +1,7 @@
 # Set regular db
 echo $DB_TEST_NAME
 if [[ -z "${DB_TEST_NAME}" ]] && [[ "${NODE_ENV}"!='production' ]]; then
-  echo "Configure only production DB"
+  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");'
 else
-  echo "Configure both dbs"
+  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");db.auth("user","password");db_ex = conn.getDB("expenses-tracker-local-test");db_ex.createUser({user: "extracker",pwd: "password",roles: [ { role: "dbOwner", db: "expenses-tracker-local-test" }, "dbOwner" ]})'
 fi
