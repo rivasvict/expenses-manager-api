931db089105996bbc999a1a8b7f5f8dcd20123f1
Victor <vctr90w@gmail.com>
2019-02-26 23:03:34 -0400

    First approach to right db config

--- docker-config/mongo/configDbs.sh
+++ docker-config/mongo/configDbs.sh
@@ -1,7 +1,10 @@
 # Set regular db
 echo $DB_TEST_NAME
 if [[ -z "${DB_TEST_NAME}" ]] && [[ "${NODE_ENV}"!='production' ]]; then
-  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");'
+  #mongo --eval 'conn = new Mongo();db = conn.getDB("admin");'
+  echo "uno";
 else
-  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");db.auth("user","password");db_ex = conn.getDB("expenses-tracker-local-test");db_ex.createUser({user: "extracker",pwd: "password",roles: [ { role: "dbOwner", db: "expenses-tracker-local-test" }, "dbOwner" ]})'
+  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");db.auth("'${DB_ADMIN_USER}'","'${DB_ADMIN_PASSWORD}'");db_ex = conn.getDB("'${DB_TEST_NAME}'");db_ex.createUser({user: "'${DB_TEST_USER}'",pwd: "'${DB_TEST_PASSWORD}'",roles: [ { role: "dbOwner", db: "'${DB_TEST_NAME}'" }, "dbOwner" ]})'
+  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");db.auth("'${DB_ADMIN_USER}'","'${DB_ADMIN_PASSWORD}'");db_ex = conn.getDB("'${DB_NAME}'");db_ex.createUser({user: "'${DB_USER}'",pwd: "'${DB_PASSWORD}'",roles: [ { role: "dbOwner", db: "'${DB_NAME}'" }, "dbOwner" ]})'
+  echo "hola";
 fi
