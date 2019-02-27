10702f0d008ae192cc4e3be21022181ef6eba0f8
Victor <vctr90w@gmail.com>
2019-02-26 23:37:55 -0400

    Added all users config for db

--- docker-config/mongo/configDbs.sh
+++ docker-config/mongo/configDbs.sh
@@ -1,10 +1,15 @@
 # Set regular db
 echo $DB_TEST_NAME
+
+DB_ADMIN_CONNECTION='conn = new Mongo();db = conn.getDB("admin");db.auth("'${DB_ADMIN_USER}'","'${DB_ADMIN_PASSWORD}'");';
+CONFIG_DB_REGULAR_USER='db_ex = conn.getDB("'${DB_NAME}'");db_ex.createUser({user: "'${DB_USER}'",pwd: "'${DB_PASSWORD}'",roles: [ { role: "dbOwner", db: "'${DB_NAME}'" }, "dbOwner" ]})';
+CONFIG_DB_TEST_USER='db_ex_test = conn.getDB("'${DB_TEST_NAME}'");db_ex_test.createUser({user: "'${DB_TEST_USER}'",pwd: "'${DB_TEST_PASSWORD}'",roles: [ { role: "dbOwner", db: "'${DB_TEST_NAME}'" }, "dbOwner" ]})';
+CREATE_USER_DB="$DB_ADMIN_CONNECTION$CONFIG_DB_REGULAR_USER";
+CREATE_USER_TEST_DB="$DB_ADMIN_CONNECTION$CONFIG_DB_TEST_USER";
+
 if [[ -z "${DB_TEST_NAME}" ]] && [[ "${NODE_ENV}"!='production' ]]; then
-  #mongo --eval 'conn = new Mongo();db = conn.getDB("admin");'
-  echo "uno";
+  mongo --eval "${CREATE_USER_DB}"
 else
-  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");db.auth("'${DB_ADMIN_USER}'","'${DB_ADMIN_PASSWORD}'");db_ex = conn.getDB("'${DB_TEST_NAME}'");db_ex.createUser({user: "'${DB_TEST_USER}'",pwd: "'${DB_TEST_PASSWORD}'",roles: [ { role: "dbOwner", db: "'${DB_TEST_NAME}'" }, "dbOwner" ]})'
-  mongo --eval 'conn = new Mongo();db = conn.getDB("admin");db.auth("'${DB_ADMIN_USER}'","'${DB_ADMIN_PASSWORD}'");db_ex = conn.getDB("'${DB_NAME}'");db_ex.createUser({user: "'${DB_USER}'",pwd: "'${DB_PASSWORD}'",roles: [ { role: "dbOwner", db: "'${DB_NAME}'" }, "dbOwner" ]})'
-  echo "hola";
+  mongo --eval "${CREATE_USER_DB}";
+  mongo --eval "${CREATE_USER_TEST_DB}"
 fi
