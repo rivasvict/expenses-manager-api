b0a533ff9121152a28f284179d4466c2a448d1ed
Victor <vctr90w@gmail.com>
2019-04-08 22:56:23 -0400

    Abstracted the way to test authentication handlers

--- routes/authentication/index.spec.js
+++ routes/authentication/index.spec.js
@@ -7,45 +7,54 @@ const loginRouteHandler = loginRouter.__get__('loginRouteHandler');
 const authenticationModule = require('../../modules/authentication.js');
 
 describe('Authentication routes', function () {
-  beforeEach('Prepare user to authenticate', function () {
-    this.userToAuthenticate = {
-      firstName: 'firstName',
-      lastName: 'lastName',
-      email: 'victor@gmail.com',
-      password: 'password'
+  beforeEach('Initialize spies for authentication', function () {
+    this.initializeSpiesForAuthentication = (user) => {
+      this.rec = {
+        body: user
+      };
+      this.res = {
+        status: () => this.res,
+        json: sinon.spy()
+      };
+      sinon.spy(this.res, 'status');
     };
-
-    this.rec = {
-      body: this.userToAuthenticate
-    };
-    this.res = {
-      status: () => this.res,
-      json: sinon.spy()
-    };
-    sinon.spy(this.res, 'status');
   });
 
-  it('loginRouteHandler: Should successfully respond to client when correct credentials passed on body', async function () {
-    const userToken = authenticationModule.getToken({ payload: this.userToAuthenticate });
-    const verifyUserStub = sinon.stub(authenticationModule, 'verifyAuthenticUser').returns(Promise.resolve(userToken));
-    await loginRouteHandler(this.rec, this.res);
-    expect(this.res.status.calledWith(200)).to.be.equal(true);
-    expect(this.res.json.calledWith({ userToken })).to.be.equal(true);
-    expect(this.res.status.callCount).to.be.equal(1);
-    expect(this.res.json.callCount).to.be.equal(1);
+  describe('loginRouteHandler tests', function () {
+    beforeEach('Prepare user to authenticate', function () {
+      this.userToAuthenticate = {
+        firstName: 'firstName',
+        lastName: 'lastName',
+        email: 'victor@gmail.com',
+        password: 'password'
+      };
+      
+      this.initializeSpiesForAuthentication(this.userToAuthenticate);
+    });
 
-    verifyUserStub.restore();
-  });
+    it('loginRouteHandler: Should successfully respond to client when correct credentials passed on body', async function () {
+      const userToken = authenticationModule.getToken({ payload: this.userToAuthenticate });
+      const verifyUserStub = sinon.stub(authenticationModule, 'verifyAuthenticUser').returns(Promise.resolve(userToken));
+      await loginRouteHandler(this.rec, this.res);
+      expect(this.res.status.calledWith(200)).to.be.equal(true);
+      expect(this.res.json.calledWith({ userToken })).to.be.equal(true);
+      expect(this.res.status.callCount).to.be.equal(1);
+      expect(this.res.json.callCount).to.be.equal(1);
 
-  it('loginRouteHandler: Should respond with error to client when correct credentials passed on body', async function () {
-    const verifyUserStub = sinon.stub(authenticationModule, 'verifyAuthenticUser').returns(Promise.resolve(null));
-    await loginRouteHandler(this.rec, this.res);
-    expect(this.res.status.calledWith(403)).to.be.equal(true);
-    expect(this.res.json.calledWith({ message: 'Invalid credentials' })).to.be.equal(true);
-    expect(this.res.status.callCount).to.be.equal(1);
-    expect(this.res.json.callCount).to.be.equal(1);
+      verifyUserStub.restore();
+    });
 
-    verifyUserStub.restore();
+    it('loginRouteHandler: Should respond with error to client when correct credentials passed on body', async function () {
+      const verifyUserStub = sinon.stub(authenticationModule, 'verifyAuthenticUser').returns(Promise.resolve(null));
+      await loginRouteHandler(this.rec, this.res);
+      expect(this.res.status.calledWith(403)).to.be.equal(true);
+      expect(this.res.json.calledWith({ message: 'Invalid credentials' })).to.be.equal(true);
+      expect(this.res.status.callCount).to.be.equal(1);
+      expect(this.res.json.callCount).to.be.equal(1);
+
+      verifyUserStub.restore();
+    });
+
   });
 
   afterEach('Restore spies', function () {
