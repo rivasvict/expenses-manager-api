02de7a028216946c8cf24abaf800169b48229959
Victor <vctr90w@gmail.com>
2019-05-09 23:15:08 -0400

    Added set handling in test

--- src/server/modules/cache.js
+++ src/server/modules/cache.js
@@ -1,14 +1,22 @@
+const Redis = require('ioredis');
+
+const config = require('./../../../config.js');
+
 class Cache {
-  constructor({ cacheClient }) {
-    this.cacheClient = cacheClient;
+  constructor() {
+    this.cacheClient = new Redis({
+      port: parseInt(config.REDIS_PORT),
+      host: config.REDIS_SERVER
+    });
+    debugger;
   }
 
   addToSet({ setName, members }) {
-    return this.cacheClient.addToSet(setName, members);
+    return this.cacheClient.sadd(setName, members);
   }
 
   isMemberOfSet({ setName, member }) {
-    return this.cacheClient.isMemberOfSet(setName, member);
+    return this.cacheClient.sismember(setName, member);
   }
 }
 
--- src/server/modules/cache.spec.js
+++ src/server/modules/cache.spec.js
@@ -1,8 +1,39 @@
+const sinon = require('sinon');
+const Redis = require('ioredis');
 const Cache = require('./cache.js');
-const cache = new Cache();
 
 describe('Save set element', function () {
+  beforeEach('Prepare cacheCLient for testing', async function () {
+    const testSet = new Set();
+    testSet.add('one');
+    testSet.add('three');
+    const sadd = async (setName, members) => {
+      const initialSizeOfSet = testSet.size;
+      members.forEach((member) => {
+        testSet.add(member);
+      });
+
+      if (testSet.size !== (initialSizeOfSet + members.length)) {
+        throw new Error();
+      } else if (testSet.size === (initialSizeOfSet + members.length)) {
+        return 'Success';
+      }
+    };
+    this.cacheClientConstructorStub = await sinon.createStubInstance(Redis, {
+      //TODO: Make sure to makke these two functions work as
+      //they would do in Redis with the testSet previously created
+      sadd: sinon.stub().returns(Promise.resolve('Success')),
+      sismember: sinon.stub().returns(Promise.resolve('Success'))
+    });
+    this.cache = sinon.createStubInstance(Cache);
+    this.cache.cacheClient = this.cacheClientConstructorStub;
+  });
+
   it.only('Should return success when there is no existing element in the set of the same value', function () {
+    try {
+    } catch (error) {
+      throw error;
+    }
   });
 
   it.skip('Should return an error and do not persist if element of a set already exists');
