93da7c527c250677d7c3912af06b80b397bc4345
Victor <vctr90w@gmail.com>
2019-08-11 14:37:00 -0400

    Added constant for account model

--- src/server/models/account.js
+++ src/server/models/account.js
@@ -1,7 +1,8 @@
 const { mongoose, accountSchema } = require('../db/schemas/account/');
+const constants = require('../constants');
 
 const existingAccountModel = mongoose && mongoose.models && mongoose.models.Account;
-const DbAccount = existingAccountModel || mongoose.model('Account', accountSchema);
+const DbAccount = existingAccountModel || mongoose.model(constants.MODEL_NAMES.ACCOUNT, accountSchema);
 class Account extends DbAccount {
   constructor(account) {
     super(account);
