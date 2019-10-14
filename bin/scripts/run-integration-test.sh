8361c90e95a0edae5ec17de64c6601aa06a9095a
Victor <vctr90w@gmail.com>
2019-10-13 21:09:42 -0400

    Improved npm scripts for integration testing purposes

--- bin/scripts/run-integration-test.sh
+++ bin/scripts/run-integration-test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-nvm use
-
 docker-compose up -d;
 MONGO_CONTAINER_ID=''
 while [[ $MONGO_CONTAINER_ID == '' ]]; do
@@ -12,5 +10,5 @@ until docker exec -itd $MONGO_CONTAINER_ID grep -q 'waiting for connections on p
   echo "Waiting for container to be up"
   sleep 0.1
 done
-npm run integration-test;
+INTEGRATION_TEST=true DEBUG=true node_modules/mocha/bin/mocha "$1" "$2" ./test/integration/**/*.spec.js ./test/integration/**/**/*.spec.js
 docker-compose down;
